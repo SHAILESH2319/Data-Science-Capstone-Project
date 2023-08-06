70573f00079132a57260a86cb6fae016944a7612
SHAILESH2319 <131481627+SHAILESH2319@users.noreply.github.com>
2023-08-06 11:34:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,46 +24,47 @@ def main():
     fuel=(df['fuel'].unique())
 
     p2=st.slider('Model Year',2005,2020,2005)
+    p3=(st.slider('KM Driven',500,10000000,500))
 
-    p3=st.selectbox('Seller Type',seller)
-    if p3=='Individual':
-        p3=1
-    elif p3=='Dealer':
-        p3=0
-    elif p3=='Trustmark Dealer':
-        p3=2
-
-    p4=st.selectbox('Owner Type',owner)
-    if p4=='First Owner':
-        p4=0
-    elif p4=='Second Owner':
-        p4=2
-    elif p4=='Third Owner':
-        p4=4
-    elif p4=='Fourth & Above Owner':
+    p4=st.selectbox('Seller Type',seller)
+    if p4=='Individual':
         p4=1
-    elif p4=='Test Drive Car':
-        p4=3
+    elif p4=='Dealer':
+        p4=0
+    elif p4=='Trustmark Dealer':
+        p4=2
 
-    p5=st.selectbox('Transmission Type',transmission)
-    if p5=='Manual':
-        p5=1
-    elif p5=='Automatic':
+    p5=st.selectbox('Owner Type',owner)
+    if p5=='First Owner':
         p5=0
+    elif p5=='Second Owner':
+        p5=2
+    elif p5=='Third Owner':
+        p5=4
+    elif p5=='Fourth & Above Owner':
+        p5=1
+    elif p5=='Test Drive Car':
+        p5=3
 
-    p6=st.selectbox('Fuel Type',fuel)
-    if p6=='Petrol':
-        p6=4
-    elif p6=='Diesel':
+    p6=st.selectbox('Transmission Type',transmission)
+    if p6=='Manual':
         p6=1
-    elif p6=='CNG':
+    elif p6=='Automatic':
         p6=0
-    elif p6=='LPG':
-        p6=3
-    elif p6=='Electric':
-        p6=2
 
-    p7=(st.slider('KM Driven',500,10000000,500))
+    p7=st.selectbox('Fuel Type',fuel)
+    if p7=='Petrol':
+        p7=4
+    elif p7=='Diesel':
+        p7=1
+    elif p7=='CNG':
+        p7=0
+    elif p7=='LPG':
+        p7=3
+    elif p7=='Electric':
+        p7=2
+
+    
 
     x=pd.DataFrame({'year':[p2],'km_driven':[p7],'fuel':[p6],'seller_type':[p3],
                     'transmission':[p5],'owner':[p4]})
